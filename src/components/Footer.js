81f230370ece92583e1a6d263a9210b30ed47b98
HFTLabs <55708865+HFTLabs@users.noreply.github.com>
2021-05-25 23:09:29 +0530

    change footer info
    change footer info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,20 +5,7 @@ function Footer() {
       <footer className="footer text-center">
         <div className="container">
           <div className="row">
-            <div className="col-lg-4 mb-5 mb-lg-0">
-              <h6 className="text-uppercase mb-4">
-                <u>Verified Smart Contract</u>
-              </h6>
-              <p className="text-uppercase mb-4">
-                <a
-                  className="text-white"
-                  href="https://twitter.com/meebit_avatars"
-                  target="_blank"
-                >
-                  View Smart Contract
-                </a>
-              </p>
-            </div>
+            <div className="col-lg-4 mb-5 mb-lg-0"></div>
             <div className="col-lg-4 mb-5 mb-lg-0">
               <h4 className="text-uppercase mb-4">Around the Web</h4>
               <a
@@ -35,20 +22,6 @@ function Footer() {
               >
                 <i className="fab fa-fw fa-discord"></i>
               </a>
-            </div>
-            <div className="col-lg-4 mb-5 mb-lg-0">
-              <h6 className="text-uppercase mb-4">
-                <u>Lives on the Blockchain and IPFS</u>
-              </h6>
-              <p className="text-uppercase mb-4">
-                <a
-                  className="text-white"
-                  href="https://twitter.com/meebit_avatars"
-                  target="_blank"
-                >
-                  Record and Proof
-                </a>
-              </p>
             </div>
           </div>
         </div>
