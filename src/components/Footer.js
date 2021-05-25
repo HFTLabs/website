13a56abb5ee92907cd21662133e48c0366b043bf
HFTLabs <55708865+HFTLabs@users.noreply.github.com>
2021-05-25 18:25:54 +0530

    updating with smart contract and ipfs details
    updating with smart contract and ipfs details

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,17 @@ function Footer() {
         <div className="container">
           <div className="row">
             <div className="col-lg-4 mb-5 mb-lg-0">
-              <h4 className="text-uppercase mb-4"></h4>
-              <p className="lead mb-0">
-                <br />
+              <h6 className="text-uppercase mb-4">
+                <u>Verified Smart Contract</u>
+              </h6>
+              <p className="text-uppercase mb-4">
+                <a
+                  className="text-white"
+                  href="https://twitter.com/meebit_avatars"
+                  target="_blank"
+                >
+                  View Smart Contract
+                </a>
               </p>
             </div>
             <div className="col-lg-4 mb-5 mb-lg-0">
@@ -20,9 +28,27 @@ function Footer() {
               >
                 <i className="fab fa-fw fa-twitter"></i>
               </a>
-              <a className="btn btn-outline-light btn-social mx-1" href="https://discord.gg/m4MPstgTNd" target="_blank">
+              <a
+                className="btn btn-outline-light btn-social mx-1"
+                href="https://discord.gg/m4MPstgTNd"
+                target="_blank"
+              >
                 <i className="fab fa-fw fa-discord"></i>
               </a>
+            </div>
+            <div className="col-lg-4 mb-5 mb-lg-0">
+              <h6 className="text-uppercase mb-4">
+                <u>Lives on the Blockchain and IPFS</u>
+              </h6>
+              <p className="text-uppercase mb-4">
+                <a
+                  className="text-white"
+                  href="https://twitter.com/meebit_avatars"
+                  target="_blank"
+                >
+                  Record and Proof
+                </a>
+              </p>
             </div>
           </div>
         </div>
